1231092e0ae79c7bb9511dd62b5acac7e7be5f45
Tundra Work <tundrawork@gmail.com>
2018-08-03 15:59:22 +0800

    add missed files

---
+++ env.js
@@ -0,0 +1,14 @@
+module.exports = {
+    prod: {
+        vega_host: 'https://bangumi.co',
+        extension_id: 'jjaofkdpddbhheppagkjkodpddlcnhle',
+        extension_name: '番組Collection Plus',
+        extension_description: 'Watch bangumi, in the way it should be.'
+    },
+    dev: {
+        vega_host: 'https://staging.internal.railgun.co',
+        extension_id: 'blgnabmjbkgkbdkjaflgghbpggjnapbi',
+        extension_name: '番組Collection Plus',
+        extension_description: 'Watch bangumi, in the way it should be.'
+    }
+};
